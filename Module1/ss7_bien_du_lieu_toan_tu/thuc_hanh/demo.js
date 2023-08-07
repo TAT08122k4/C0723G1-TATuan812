316c58aca20081d98d8a7e27d78a26d66cfb9bd7
unknown <tbin08122004@gmail.com>
2023-08-07 15:36:04 +0700

    updatebaitapss7

---
+++ Module1/ss7_bien_du_lieu_toan_tu/thuc_hanh/demo.js
@@ -0,0 +1,7 @@
+let inputWidth = +prompt("Input Width");
+let inputLength = +prompt("Input Length")
+let area = inputWidth * inputLength;
+document.write("Enter Width:" + inputWidth);
+document.write("<br>")
+document.write("Enter Length:" + inputLength);
+document.write("Area = " + area);
