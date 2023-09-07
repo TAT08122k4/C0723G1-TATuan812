aaf041dcfd30b657c27b177a23912afd367ebbd7
unknown <tbin08122004@gmail.com>
2023-09-07 13:49:51 +0700

    updatebaitap03

--- Module2/module_2/src/ss3_array/bai_tap/CharCount.java
+++ Module2/module_2/src/ss3_array/bai_tap/CharCount.java
@@ -1,7 +1,19 @@
 package ss3_array.bai_tap;
 
+import java.util.Scanner;
+
 public class CharCount {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String stringramdom = "swedasagwaszx";
+        System.out.println("Enter a character u wanna check: ");
+        char checkChar = sc.next().charAt(0);
+        int count = 0;
+        for (int i = 0; i < stringramdom.length(); i++) {
+            if (stringramdom.charAt(i) == checkChar) {
+                count++;
+            }
+        }
+        System.out.printf("Số Kí Tự %s Xuất Hiện Trong Chuỗi Là %d",checkChar,count);
     }
 }
