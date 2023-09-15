83a2a2023abb4c5117601b8a017a9d8bbad26d6a
unknown <tbin08122004@gmail.com>
2023-09-15 21:09:53 +0700

    upbaitap9

--- Module2/module_2/src/ss9_dsa_danh_sach/bai_tap/implement_method_arraylist/MyList.java
+++ Module2/module_2/src/ss9_dsa_danh_sach/bai_tap/implement_method_arraylist/MyList.java
@@ -2,7 +2,6 @@ package ss9_dsa_danh_sach.bai_tap.implement_method_arraylist;
 
 
 public class MyList<E> {
-    // Bài Tôi làm kệ tôi , đừng vô soi mói làm ơn (Trừ anh Hải TT)
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
     private Object elements[];
