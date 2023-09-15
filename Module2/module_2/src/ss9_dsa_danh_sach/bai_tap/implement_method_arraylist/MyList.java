1cef4057d7c6fa450019b31cd46b4221652e4684
unknown <tbin08122004@gmail.com>
2023-09-15 17:21:02 +0700

    upbaitap9

--- Module2/module_2/src/ss9_dsa_danh_sach/bai_tap/implement_method_arraylist/MyList.java
+++ Module2/module_2/src/ss9_dsa_danh_sach/bai_tap/implement_method_arraylist/MyList.java
@@ -1,22 +1,101 @@
 package ss9_dsa_danh_sach.bai_tap.implement_method_arraylist;
 
+
 public class MyList<E> {
+    // Bài Tôi làm kệ tôi , đừng vô soi mói làm ơn (Trừ anh Hải TT)
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
     private Object elements[];
 
     public MyList (){
+        elements = new Object[DEFAULT_CAPACITY];
     }
+
+
     public MyList(int capacity){
      elements = new Object[capacity];
     }
     void add(int index , E element){
-
+     if (index < 0|| index > size){
+         throw new IndexOutOfBoundsException();
+     } else if(index== size){
+         ensureCapacity();
+     } else {
+         for (int i = size; i >= index; i--) {
+             elements[i + 1] = elements[i];
+         }
+         elements[index] = element;
+         size++;
+     }
     }
-    void ensureCapacity(int minCapacity){
-        if(size >= elements.length){
-
+    boolean add(E element){
+        if (size >= elements.length){
+            return false;
         }
+        elements[size++] = element;
+        return true;
     }
+    int length(){
+        return elements.length;
+    }
+    int size(){
+        return size;
+    }
+    E get(int i){
+        if (i < 0 || i > size()){
+            return null;
+        }
+        return (E) elements[i];
+    }
+    boolean isEmpty(){
+        if (size == 0){
+            return true;
+        }
+        return false;
+    }
+   protected Object clone(){
+        Object[] newArr = new Object[size];
+        for (int i = 0; i < size(); i++) {
+            newArr[i] = elements[i];
+        }
+        return newArr;
+    }
+    E remove(int index){
+        for (int i = index; i < size() ; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        return (E) elements[index];
+
+    }
+    void clear (){
+        for (int i = 0; i <= size(); i++) {
+          elements[i] = null;
+        }
+        size = 0;
+    }
+    int indexOf(E o){
+        for (int i = 0; i < size(); i++) {
+            if (o == elements[i]){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    boolean contains(E o){
+        for (int i = 0; i < size(); i++) {
+            if(o==elements[i]){
+                return true;
+            }
+        }
+        return false;
+
+    }
+    void ensureCapacity(){
+           elements = new Object[elements.length + elements.length/2];
+    }
+
 
 }
---
+++ Module2/module_2/src/ss9_dsa_danh_sach/bai_tap/implement_method_arraylist/MyListTest.java
@@ -0,0 +1,41 @@
+package ss9_dsa_danh_sach.bai_tap.implement_method_arraylist;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> myList = new MyList<>(14);
+        myList.add(10);
+        myList.add(1);
+        myList.add(0);
+        myList.add(2);
+        myList.add(7);
+        myList.add(100);
+        myList.add(101);
+        myList.add(102);
+        myList.add(3,103);
+        for (int i = 0; i <= myList.length(); i++) {
+            System.out.println(myList.get(i));
+        }
+        System.out.println(myList.contains(3));
+
+        Object[] e = (Object[]) myList.clone();
+        System.out.println(Arrays.toString(e));
+        myList.remove(5);
+        Object[] e1 = (Object[]) myList.clone();
+        System.out.println(Arrays.toString(e1));
+        myList.clear();
+        for (int i = 0; i <= myList.size(); i++) {
+            System.out.println(myList.get(i));
+        }
+        System.out.println(myList.size());
+        System.out.println(myList.indexOf(100));
+        System.out.println(myList.isEmpty());
+
+
+
+
+    }
+}
