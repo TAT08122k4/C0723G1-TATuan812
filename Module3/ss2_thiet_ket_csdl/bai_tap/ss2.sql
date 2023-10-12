470235d64bde4a7efd884b883bda990327571d39
unknown <tbin08122004@gmail.com>
2023-10-12 13:58:07 +0700

    ss2 module 3

---
+++ Module3/ss2_thiet_ket_csdl/bai_tap/ss2.sql
@@ -0,0 +1,28 @@
+drop database if exists quan_ly_ban_hang;
+create database  if  not exists quan_ly_ban_hang; 
+use quan_ly_ban_hang;
+create table customer(
+customer_id int primary key auto_increment,
+customer_name varchar(50),
+customer_age varchar(50)
+);
+create table orders(
+order_id int primary key,
+customer_id int,
+order_date datetime,
+order_total_price int,
+foreign key(customer_id) references customer(customer_id)
+);
+create table product(
+product_id int primary key auto_increment,
+product_name varchar(50),
+product_price int
+);
+create table order_detail(
+order_id int,
+product_id int,
+order_soluong int,
+primary key(order_id,product_id),
+foreign key(order_id) references orders(order_id),
+foreign key(product_id) references product(product_id)  
+);
