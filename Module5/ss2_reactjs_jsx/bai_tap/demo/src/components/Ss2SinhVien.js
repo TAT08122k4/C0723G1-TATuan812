3aba5ba7628cb15a3b2935b9633cf5696e19f647
TAT08122k4 <140997922+TAT08122k4@users.noreply.github.com>
2024-01-24 13:59:56 +0700

    fix ss1 ss2

--- Module5/ss2_reactjs_jsx/bai_tap/demo/src/components/Ss2SinhVien.js
+++ Module5/ss2_reactjs_jsx/bai_tap/demo/src/components/Ss2SinhVien.js
@@ -2,44 +2,54 @@ import React, { Children } from 'react'
 import '../styles/Ss2SinhVien.css'
 const students = [
     {
+      id: 1,
         company: 'Alfreds Futterkiste',
         contact: 'Maria Anders',
         country: 'Germany'
       },
       {
+        id: 2,
         company: 'Centro comercial Moctezuma',
         contact: 'Francisco Chang',
         country: 'Mexico'
       },
       {
+        id: 3,
         company: 'Ernst Handel',
         contact: 'Roland Mendel',
         country: 'Austria'
       },
       {
+        id: 4,
         company: 'Island Trading',
         contact: 'Helen Bennett',
         country: 'UK'
       },
       {
+        id: 5,
         company: 'Laughing Bacchus Winecellars',
         contact: 'Yoshi Tannamuri',
         country: 'Canada'
       },
       {
+        id: 6,
         company: 'Magazzini Alimentari Riuniti',
         contact: 'Giovanni Rovelli',
         country: 'Italy'
       }
 ]
-function DisplayStudent(){
-  return (<>{students.map(student => (
-    <tr>
-        <td>{student.company}</td>
-        <td>{student.contact}</td>
-        <td>{student.country}</td>
-    </tr>
-))}</>)
+function DisplayStudent() {
+  return (
+    <>
+      {students.map((student) => (
+        <tr key={student.id}>
+          <td>{student.company}</td>
+          <td>{student.contact}</td>
+          <td>{student.country}</td>
+        </tr>
+      ))}
+    </>
+  );
 }
 
-export default DisplayStudent
+export default DisplayStudent;
